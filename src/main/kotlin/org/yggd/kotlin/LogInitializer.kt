ecf1545f95fb69f1845cdf9f4fa386c23031e074
Mitsuyoshi Hasegawa <yggd@yahoo.co.jp>
2016-05-28 05:19:03 +0900

    remove dependencies spek and kunit. and update version to 1.0

--- src/main/kotlin/org/yggd/kotlin/LogInitializer.kt
+++ src/main/kotlin/org/yggd/kotlin/LogInitializer.kt
@@ -4,12 +4,26 @@ import org.apache.commons.logging.LogFactory
 import org.apache.log4j.Logger
 import org.slf4j.LoggerFactory
 
-/*
- * Log Initializer
+/**
+ * Obtain SLF4J logger instance.
+ * logger will be named by caller java class.
+ *
+ * @return logger instance of org.slf4j.Logger
  */
-
 fun <T : Any> T.forSlf4j() : org.slf4j.Logger = LoggerFactory.getLogger(this.javaClass)
 
+/**
+ * Obtain Apache Commons logging (JCL) logger instance.
+ * logger will be named by caller java class.
+ *
+ * @return logger instance of org.apache.commons.logging.Log
+ */
 fun <T : Any> T.forJcl() : org.apache.commons.logging.Log = LogFactory.getLog(this.javaClass)
 
+/**
+ * Obtain Log4J logger instance.
+ * logger will be named by caller java class.
+ *
+ * @return logger instance of org.apache.log4j.Logger
+ */
 fun <T : Any> T.forLog4j() = Logger.getLogger(this.javaClass)
--- src/test/kotlin/org/yggd/kotlin/LogInitializerTest.kt
+++ src/test/kotlin/org/yggd/kotlin/LogInitializerTest.kt
@@ -6,9 +6,8 @@ import org.apache.log4j.Level
 import org.apache.log4j.LogManager
 import org.apache.log4j.spi.LoggingEvent
 import org.hamcrest.CoreMatchers.*
-import org.jetbrains.spek.api.DescribeParser
-import org.jetbrains.spek.api.Spek
 import org.junit.Assert.*
+import org.junit.Test
 import org.mockito.ArgumentCaptor
 import org.mockito.Mockito.*
 import uk.org.lidalia.slf4jtest.LoggingEvent.*
@@ -16,46 +15,49 @@ import uk.org.lidalia.slf4jtest.TestLoggerFactory
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 
-class LogInitializerTest : Spek({
-    given("some logger test") {
-        on("test for slf4j") {
-            val testLogger = TestLoggerFactory.getTestLogger(DescribeParser::class.java)
+open class LogInitializerTest {
+
+    @Test
+    open fun testForSlf4j() {
+            val testLogger = TestLoggerFactory.getTestLogger(LogInitializerTest::class.java)
             val loggerSlf4j = forSlf4j()
 
             loggerSlf4j.warn("test slf4j")
 
-            assertThat(testLogger.loggingEvents, `is`(listOf(warn("test slf4j"))))
-        }
-        on("test for jcl") {
-            val logJcl = forJcl() as SimpleLog
+            assertThat("slf4j log message and level", testLogger.loggingEvents, `is`(listOf(warn("test slf4j"))))
+    }
 
-            val errOrg = System.err
-            val errTest = ByteArrayOutputStream()
-            // stderr replace
-            System.setErr(PrintStream(errTest))
-            try {
-               logJcl.error("test jcl")
+    @Test
+    open fun testForJcl() {
+        val logJcl = forJcl() as SimpleLog
 
-                assertThat(errTest.toString(System.getProperty("file.encoding")),
-                        containsString("[ERROR] DescribeParser - test jcl"))
-            } finally {
-                System.setErr(errOrg)
-                errTest.close()
-            }
+        val errOrg = System.err
+        val errTest = ByteArrayOutputStream()
+        // stderr replace
+        System.setErr(PrintStream(errTest))
+        try {
+            logJcl.error("test jcl")
 
-        }
-        on("test for log4j") {
-            val captor = ArgumentCaptor.forClass(LoggingEvent::class.java)
-            val appender = mock(Appender::class.java)
-            LogManager.getRootLogger().addAppender(appender)
-
-            val loggerlog4j = forLog4j()
-            loggerlog4j.info("test log4j")
-
-            verify(appender).doAppend(captor.capture())
-            val event = captor.value
-            assertThat(event.getLevel(), `is`(Level.INFO))
-            assertThat(event.renderedMessage, `is`("test log4j"))
+            assertThat(errTest.toString(System.getProperty("file.encoding")),
+            containsString("[ERROR] LogInitializerTest - test jcl"))
+        } finally {
+            System.setErr(errOrg)
+            errTest.close()
         }
     }
-})
+
+    @Test
+    open fun testForLog4j() {
+        val captor = ArgumentCaptor.forClass(LoggingEvent::class.java)
+        val appender = mock(Appender::class.java)
+        LogManager.getRootLogger().addAppender(appender)
+
+        val loggerlog4j = forLog4j()
+        loggerlog4j.info("test log4j")
+
+        verify(appender).doAppend(captor.capture())
+        val event = captor.value
+        assertThat(event.getLevel(), `is`(Level.INFO))
+        assertThat(event.renderedMessage, `is`("test log4j"))
+    }
+}
